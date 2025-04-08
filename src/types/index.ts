923dbecd84a9d2abe23a008ff43636685f336acf
Родионцева Ирина Михайловна <I.rodiontseva165@yandex.ru>
2025-04-08 20:29:49 +0300

    readme is ready

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface IProduct {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+}
+export type TProductCard = Omit<IProduct, 'description'>
+
+export type TProductPreview = IProduct
+
+export type TProductInBasket = Pick<IProduct, 'id' | 'title' | 'price'>
+
+export type PaymentMethod = 'cash' | 'card'
+
+export interface IOrder {
+  payment: PaymentMethod;
+  email: string;
+  phone: string;
+  address: string;
+  total: number;
+  items: string[];
+}
+
+export type TOrderInfo = Omit<IOrder, 'items'>
+
+export type TOrderData = Pick<IOrder, 'payment' | 'address'>
+
+export type TOrderContacts = Pick<IOrder, 'email' | 'phone'>
+
+export interface IOrderResult {
+  id: string;
+  total: number;
+}
+
+
+
+
+
